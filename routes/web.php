de6e585b8d091ae90f74b62ef44d6b7f1f9c292a
syedmuzammilabbas <55852619+ItsSalman99@users.noreply.github.com>
2025-12-29 07:00:29 +0500

    routes changes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\Company\TechnicianController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthenticationController::class, 'login'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'loginStore'])->name('login.store');
+Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
 
 Route::prefix('portal')
     ->middleware('auth-check')
@@ -14,6 +17,14 @@ Route::prefix('portal')
 
         Route::get('/', [DashboardController::class, 'index'])
             ->name('dashboard');
+
+        Route::prefix('technicians')->name('technicians.')
+        ->group(function () {
+
+            Route::get('/', [TechnicianController::class, 'index'])
+            ->name('index');
+
+        });
 
     });
 
