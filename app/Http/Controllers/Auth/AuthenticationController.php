3ffd99112116cbc853066d7fb2465fed298c2f53
syedmuzammilabbas <55852619+ItsSalman99@users.noreply.github.com>
2025-12-29 06:59:41 +0500

    auth changes

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -62,4 +62,17 @@ class AuthenticationController extends Controller
         ]);
 
     }
+
+    public function logout(Request $request)
+    {
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+
+        return redirect()->route('login');
+
+    }
+
 }
