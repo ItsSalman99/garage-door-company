d21022fb03f492653fd6ff08f406fbbbf73ccb6e
Salman Abbas <55852619+ItsSalman99@users.noreply.github.com>
2026-01-13 02:28:01 +0500

    routes changes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticationController;
-use App\Http\Controllers\Company\ProductCategoryController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Company\ProductController;
-use App\Http\Controllers\Company\ServiceCategoryController;
 use App\Http\Controllers\Company\ServiceController;
 use App\Http\Controllers\Company\TechnicianController;
 use App\Http\Controllers\DashboardController;
@@ -14,7 +13,6 @@ Route::get('/', [AuthenticationController::class, 'index'])->name('index');
 Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'loginStore'])->name('login.store');
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
-
 
 Route::get('/admin/login', [AuthenticationController::class, 'adminLogin'])->name('adminlogin');
 
@@ -40,16 +38,16 @@ Route::prefix('portal')
 
             });
 
-        Route::prefix('products')->name('products.')
+        Route::prefix('categories')->name('categories.')
             ->group(function () {
 
-                Route::prefix('categories')->name('categories.')
-                    ->group(function () {
+                Route::get('/', [CategoryController::class, 'index'])
+                    ->name('index');
 
-                        Route::get('/', [ProductCategoryController::class, 'index'])
-                            ->name('index');
+            });
 
-                    });
+        Route::prefix('products')->name('products.')
+            ->group(function () {
 
                 Route::get('/', [ProductController::class, 'index'])
                     ->name('index');
@@ -58,14 +56,6 @@ Route::prefix('portal')
 
         Route::prefix('services')->name('services.')
             ->group(function () {
-
-                Route::prefix('categories')->name('categories.')
-                    ->group(function () {
-
-                        Route::get('/', [ServiceCategoryController::class, 'index'])
-                            ->name('index');
-
-                    });
 
                 Route::get('/', [ServiceController::class, 'index'])
                     ->name('index');
@@ -79,8 +69,10 @@ Route::prefix('portal')
                     ->name('index');
                 Route::get('/show/{id}', [QuotationOrderController::class, 'show'])
                     ->name('show');
-                Route::post('/store', [QuotationOrderController::class, 'store'])
-                    ->name('store');
+                Route::post('/store/product', [QuotationOrderController::class, 'storeProduct'])
+                    ->name('storeProduct');
+                Route::post('/store/service', [QuotationOrderController::class, 'storeService'])
+                    ->name('storeService');
                 Route::get('/generateBill/{id}', [QuotationOrderController::class, 'generateBill'])
                     ->name('generateBill');
             });
